381fc469f60730409975e97fb3a8f294c42a0797
Michael Wellington <mowelling33@gmail.com>
2023-07-10 11:19:23 -0600

    I didnt follow the instructions because i didnt feel like it

---
+++ Module4/script.js
@@ -0,0 +1,11 @@
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Michael", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+for (var i = 0; i < names.length; i++) {
+  var firstLetter = names[i].charAt(0).toLowerCase();
+
+  if (firstLetter === "j") {
+    console.log("Good Bye " + names[i]);
+  } else {
+    console.log("Hello " + names[i]);
+  }
+}
